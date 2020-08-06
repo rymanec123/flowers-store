7fe55ff6544c49805a2237de215ad1cbdb2e901e
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-08-06 15:43:48 +0300

    add material cart form

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, Injectable, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {CartService} from '@app/services/cart/cart.service';
-import { FormBuilder } from '@angular/forms';
+import {FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -11,6 +11,7 @@ import { FormBuilder } from '@angular/forms';
 export class CartComponent implements OnInit, OnDestroy, AfterViewInit {
   items;
   checkoutForm;
+  id: string;
 
   constructor(
     private cartService: CartService,
@@ -19,21 +20,21 @@ export class CartComponent implements OnInit, OnDestroy, AfterViewInit {
     this.items = this.cartService.getItems();
     this.checkoutForm = this.formBuilder.group({
       name: '',
-      address: ''
+      login: ''
     });
   }
 
   onSubmit(customerData) {
 
     console.warn('Ваш заказ был отправлен', customerData);
+    alert('Ваш заказ был отправлен');
 
     this.items = this.cartService.clearCart();
     this.checkoutForm.reset();
   }
 
-  ngOnInit(): void {
-    console.log('Компонет Корзина создался');
-  }
+
+  ngOnInit() {}
 
   ngOnDestroy() {}
 
