109fdcfe416b61fdc35fdd33e5fb07ffa729930f
useyourillusions <minsk.bay@gmail.com>
2020-08-06 23:52:03 +0300

    Added a deploy config for HEROKU

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
+const { projects } = require('./angular.json');
+const distPath = projects['flowers-store'].architect.build.options.outputPath;
 const productsHandlerGet = require('./BACKEND/api-routes/products/get');
 const ordersHandlerPost = require('./BACKEND/api-routes/orders/post');
 const seedInitial = require('./BACKEND/helpers/seed-initial');
 
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, distPath)));
 
 // Products routes
 app.get('/api/products', productsHandlerGet);
@@ -19,10 +21,15 @@ app.get('/api/products', productsHandlerGet);
 // Orders routes
 app.post('/api/orders', ordersHandlerPost);
 
+// index.html route
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, distPath + '/index.html'));
+});
+
 
 seedInitial();
 
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${process.env.PORT || PORT}`)
-})
+    console.log('Running on port ' + PORT)
+});
